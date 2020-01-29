5d902ac65e85a50d9ef297f46a06e24a16739754
newleehj <humanwriting82@naver.com>
2020-01-29 21:39:16 +0900

    이미지 및 모바일 페이지 최종 수정

--- skin/board/intro_p/list.skin.php
+++ skin/board/intro_p/list.skin.php
@@ -6,10 +6,10 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 
 <style>
-    .intro_p {height:1000px;margin:50px auto;}
+    .intro_p {height:1800px;margin:50px auto;}
 </style>
 
 <div class="intro_p">
-    <img src="<?php echo G5_IMG_URL ?>/intro.jpg" alt="수강 교육 사진">
+    <img src="<?php echo G5_IMG_URL ?>/intro.png" alt="수강 교육 사진">
 </div>
 
