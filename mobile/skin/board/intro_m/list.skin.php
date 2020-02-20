96dcb6102409989c4fdced1a4527a398f60ee857
newleehj <humanwriting82@naver.com>
2020-02-20 20:07:44 +0900

    모바일 소개 페이지 수정

--- mobile/skin/board/intro_m/list.skin.php
+++ mobile/skin/board/intro_m/list.skin.php
@@ -6,10 +6,48 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 
 <style>
-    .intro_p {height:1173px;margin:30px auto;}
+    .intro_p{background:#fff;margin-bottom:50px;}
+    .intro_p>img{width:100%;}
+    .img22{width:100%;}
+    .img22>img{width:80%;margin:0 auto;}
+    .iph,.ipt{font-size:16px;line-height:1.5;}
+    .iph{color:darkblue;font-weight:bolder;}
+    .ipwrap{height:660px;margin:30px 0;text-align:center;}
 </style>
 
 <div class="intro_p">
-    <img src="<?php echo G5_IMG_URL ?>/m_intro.jpg" alt="수강 교육 사진">
+    <img src="<?php echo G5_IMG_URL ?>/intro4.jpg" alt="파주작은갤러리 전경사진">
+    <div class="ipwrap">
+        <p class="iph">▲ 파주 작은 갤러리 소개</p>
+        <p class="ipt">
+            파주 작은 갤러리는<br>
+            취미•작품•직업을 위한<br>
+            시민을 위한 사진공간입니다.<br><br>
+
+            카메라•필름•사진의<br>
+            과거와 현재를 볼 수 있는<br>
+            작은 전시장이 마련되었습니다.<br><br>
+
+            지역주민, 청소년들이<br>
+            부담없이 즐기며 관람할수 있도록<br> 
+            전시장은 무료로 개방하며
+            무료 사진강좌 및<br>
+            사진작가 초청 강좌를 진행합니다.<br><br>
+
+            소재지 :  파주시 법원읍 동문리 502-4<br>  
+            이메일 : hy8269631@hanmail.net<br>
+            연락처 : 010.3028.5580<br>
+            개관시간 : 13시-17시(월, 휴관)<br><br>
+        </p>
+        <p class="iph">▲ 파주 작은 갤러리 이운안 관장</p>
+        <p class="ipt">
+            중앙대학교 사진학과 졸업<br>
+            25년간 중앙지에서 사진기자로 재직하며<br>
+            역사의 현장과 변천사를<br>
+            사진으로 기록하였습니다.<br>
+            현재는,  한국 보도 사진가 협회 고문<br>
+            국제뉴스 취재국장 등을 역임하고 있습니다.
+        </p>
+    </div>
 </div>
 
