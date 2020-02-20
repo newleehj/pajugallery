311dc9dfc3b1fcd9c475d6cc72e4bf41e79d03a5
newleehj <humanwriting82@naver.com>
2020-02-20 19:20:13 +0900

    모바일 index.html에 슬라이드 추가

--- mobile/index.php
+++ mobile/index.php
@@ -17,7 +17,22 @@ if(defined('G5_THEME_PATH')) {
 include_once(G5_MOBILE_PATH.'/head.php');
 
 ?>
-<style>#m_main{width:420px;margin:20px auto;}</style>
+<style>
+    #m_main{width:100%;height:300px;overflow:hidden;}
+    .slideList{width:100%;height:300px;position:relative;overflow:hidden;margin:0 auto;}
+    .slideImg>img{width:100%;height:300px;display:block;}
+</style>
+
+
+
+<div id="m_main">
+    <div class="slideList">
+        <div class="slideImg"><img src="<?php echo G5_IMG_URL ?>/m_main.jpg" alt="파주작은갤러리"></div>
+        <div class="slideImg"><img src="<?php echo G5_IMG_URL ?>/m_main1.jpg" alt="파주작은갤러리"></div>
+        <div class="slideImg"><img src="<?php echo G5_IMG_URL ?>/m_main2.jpg" alt="파주작은갤러리"></div>
+    </div>
+</div>
+
 
 
 <?php 
@@ -28,10 +43,20 @@ echo latest('pic_basic', 'g_news', 4, 23);
 echo latest('pic_basic', 's_news', 4, 23);
 echo latest('basic', 'notice', 5, 23);
 
-
 ?>
 
-
+<script>
+    $(function(){
+        $('.slideList').children('div:gt(0)').hide();
+        var current=0;
+        setInterval(function(){
+            var next=(current+1)%3;
+            $('.slideList').find('div').eq(current).hide();
+            $('.slideList').find('div').eq(next).show();
+            current=next;
+         },3000)
+    })
+</script>
 
 <?php
 
